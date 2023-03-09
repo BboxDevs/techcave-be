705e515a5e75c7dcb856f45bad92f4057643f220
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-08 16:30:25 -0800

    generate types from graphql schema

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ This section is not necessarily needed in development. But just some notes inclu
 2. Run `npx graphql-codegen init`. Follow the prompts
 3. Run `yarn` to install additional dependencies added during init
 4. Run `yarn [codegen]` to generate the types where [codegen] is the answer you provided in one of the prompts from step 3 (What script in package.json should run the codegen?)
+
+**Note:** If you have implemented strict linting, you would need to add `/* eslint-disable @typescript-eslint/ban-types */` on the generated file
---
+++ src/apollo/__generated__/types.ts
@@ -0,0 +1,329 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import {
+  GraphQLResolveInfo,
+  GraphQLScalarType,
+  GraphQLScalarTypeConfig,
+} from 'graphql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Url: any;
+};
+
+export type Admin = {
+  __typename?: 'Admin';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  password: Scalars['String'];
+  role: Role;
+};
+
+export type Applicant = User & {
+  __typename?: 'Applicant';
+  email: Scalars['String'];
+  firstName: Scalars['String'];
+  id: Scalars['ID'];
+  lastName: Scalars['String'];
+  password: Scalars['String'];
+  role: Role;
+};
+
+export type Employer = User & {
+  __typename?: 'Employer';
+  company?: Maybe<Scalars['String']>;
+  email: Scalars['String'];
+  firstName?: Maybe<Scalars['String']>;
+  id: Scalars['ID'];
+  lastName?: Maybe<Scalars['String']>;
+  password: Scalars['String'];
+  role: Role;
+};
+
+export type Job = {
+  __typename?: 'Job';
+  contact: Employer;
+  description: Scalars['String'];
+  externalLink?: Maybe<Scalars['Url']>;
+  id: Scalars['ID'];
+  industry: Scalars['String'];
+  position: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  welcome?: Maybe<Welcome>;
+};
+
+export enum Role {
+  Admin = 'ADMIN',
+  Applicant = 'APPLICANT',
+  Employer = 'EMPLOYER',
+}
+
+export type User = {
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  password: Scalars['String'];
+  role: Role;
+};
+
+export type Welcome = {
+  __typename?: 'Welcome';
+  message: Scalars['String'];
+};
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
+  obj: T,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Admin: ResolverTypeWrapper<Admin>;
+  Applicant: ResolverTypeWrapper<Applicant>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Employer: ResolverTypeWrapper<Employer>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  Job: ResolverTypeWrapper<Job>;
+  Query: ResolverTypeWrapper<{}>;
+  Role: Role;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  Url: ResolverTypeWrapper<Scalars['Url']>;
+  User: ResolversTypes['Applicant'] | ResolversTypes['Employer'];
+  Welcome: ResolverTypeWrapper<Welcome>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Admin: Admin;
+  Applicant: Applicant;
+  Boolean: Scalars['Boolean'];
+  Employer: Employer;
+  ID: Scalars['ID'];
+  Job: Job;
+  Query: {};
+  String: Scalars['String'];
+  Url: Scalars['Url'];
+  User: ResolversParentTypes['Applicant'] | ResolversParentTypes['Employer'];
+  Welcome: Welcome;
+};
+
+export type AdminResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Admin'] = ResolversParentTypes['Admin']
+> = {
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type ApplicantResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Applicant'] = ResolversParentTypes['Applicant']
+> = {
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type EmployerResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Employer'] = ResolversParentTypes['Employer']
+> = {
+  company?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  firstName?: Resolver<
+    Maybe<ResolversTypes['String']>,
+    ParentType,
+    ContextType
+  >;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  lastName?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type JobResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Job'] = ResolversParentTypes['Job']
+> = {
+  contact?: Resolver<ResolversTypes['Employer'], ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  externalLink?: Resolver<
+    Maybe<ResolversTypes['Url']>,
+    ParentType,
+    ContextType
+  >;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  industry?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  position?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type QueryResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']
+> = {
+  welcome?: Resolver<Maybe<ResolversTypes['Welcome']>, ParentType, ContextType>;
+};
+
+export interface UrlScalarConfig
+  extends GraphQLScalarTypeConfig<ResolversTypes['Url'], any> {
+  name: 'Url';
+}
+
+export type UserResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']
+> = {
+  __resolveType: TypeResolveFn<
+    'Applicant' | 'Employer',
+    ParentType,
+    ContextType
+  >;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
+};
+
+export type WelcomeResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes['Welcome'] = ResolversParentTypes['Welcome']
+> = {
+  message?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Admin?: AdminResolvers<ContextType>;
+  Applicant?: ApplicantResolvers<ContextType>;
+  Employer?: EmployerResolvers<ContextType>;
+  Job?: JobResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  Url?: GraphQLScalarType;
+  User?: UserResolvers<ContextType>;
+  Welcome?: WelcomeResolvers<ContextType>;
+};
