4732197d3c431faf35ebc73cff21edb017cacaa8
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-13 19:06:05 -0700

    JOB-7 added another command on docker-compose to migrate db

---
+++ prisma/migrate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Run this to check if migration have been done.
+
+if [[ $(npx prisma migrate status | grep "Following migration have not yet been applied")  ]]; then 
+    yarn db:reset -f
+fi
